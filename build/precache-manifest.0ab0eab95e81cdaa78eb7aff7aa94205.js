23657fc8fa7df0f4a738ca7301dc556b5d0e9e08
Pravin Jagtap <Pravin.Jagtap@INFOVISIONLABS.COM>
2019-12-12 17:51:35 +0530

    Added build for shopping cart project.

---
+++ build/precache-manifest.0ab0eab95e81cdaa78eb7aff7aa94205.js
@@ -0,0 +1,42 @@
+self.__precacheManifest = [
+  {
+    "revision": "da1154611141f6eade02",
+    "url": "/static/css/main.3d897fd0.chunk.css"
+  },
+  {
+    "revision": "da1154611141f6eade02",
+    "url": "/static/js/main.da115461.chunk.js"
+  },
+  {
+    "revision": "5a9cd442fbddd457dba2",
+    "url": "/static/js/1.5a9cd442.chunk.js"
+  },
+  {
+    "revision": "229c360febb4351a89df",
+    "url": "/static/js/runtime~main.229c360f.js"
+  },
+  {
+    "revision": "6c426583a257819912278cedfdd9badf",
+    "url": "/static/media/item1.6c426583.jpg"
+  },
+  {
+    "revision": "8779dd15782203466bfb67cac99ef2ec",
+    "url": "/static/media/item2.8779dd15.jpg"
+  },
+  {
+    "revision": "9bd2546f523e4a6e2515068c65ca441c",
+    "url": "/static/media/item3.9bd2546f.jpg"
+  },
+  {
+    "revision": "65bc33d70f7f4ca793c5acc24525de9f",
+    "url": "/static/media/item4.65bc33d7.jpg"
+  },
+  {
+    "revision": "f5fd97b56137c3ed5df0f3f71482c105",
+    "url": "/static/media/item6.f5fd97b5.jpg"
+  },
+  {
+    "revision": "e6455ce1f024779fce7f33cecb5e3080",
+    "url": "/index.html"
+  }
+];
