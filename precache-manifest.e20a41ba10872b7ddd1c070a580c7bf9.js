58a7c0267581d54d9a558def55e33a5ce985bfc4
Pravin Jagtap <Pravin.Jagtap@INFOVISIONLABS.COM>
2019-12-12 18:14:33 +0530

    Updates

---
+++ precache-manifest.e20a41ba10872b7ddd1c070a580c7bf9.js
@@ -0,0 +1,42 @@
+self.__precacheManifest = [
+  {
+    "revision": "da1154611141f6eade02",
+    "url": "/react-shopping-cart/build/static/css/main.3d897fd0.chunk.css"
+  },
+  {
+    "revision": "da1154611141f6eade02",
+    "url": "/react-shopping-cart/build/static/js/main.da115461.chunk.js"
+  },
+  {
+    "revision": "5a9cd442fbddd457dba2",
+    "url": "/react-shopping-cart/build/static/js/1.5a9cd442.chunk.js"
+  },
+  {
+    "revision": "feee86a13e24259c73c0",
+    "url": "/react-shopping-cart/build/static/js/runtime~main.feee86a1.js"
+  },
+  {
+    "revision": "6c426583a257819912278cedfdd9badf",
+    "url": "/react-shopping-cart/build/static/media/item1.6c426583.jpg"
+  },
+  {
+    "revision": "8779dd15782203466bfb67cac99ef2ec",
+    "url": "/react-shopping-cart/build/static/media/item2.8779dd15.jpg"
+  },
+  {
+    "revision": "9bd2546f523e4a6e2515068c65ca441c",
+    "url": "/react-shopping-cart/build/static/media/item3.9bd2546f.jpg"
+  },
+  {
+    "revision": "65bc33d70f7f4ca793c5acc24525de9f",
+    "url": "/react-shopping-cart/build/static/media/item4.65bc33d7.jpg"
+  },
+  {
+    "revision": "f5fd97b56137c3ed5df0f3f71482c105",
+    "url": "/react-shopping-cart/build/static/media/item6.f5fd97b5.jpg"
+  },
+  {
+    "revision": "9ca1340ee4b49a3e3c9e3a07a536af5d",
+    "url": "/react-shopping-cart/build/index.html"
+  }
+];
